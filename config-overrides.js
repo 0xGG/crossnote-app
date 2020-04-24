ae7fc3cdeeb30c6c15f801fe2c5169375cfae785
Yiyi Wang <shd101wyy@gmail.com>
2020-04-24 16:01:58 +0800

    feat: Added workbox support

--- config-overrides.js
+++ config-overrides.js
@@ -3,21 +3,13 @@
 // * https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-webpack-plugin.GenerateSW#GenerateSW
 // * https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.generateSW
 
-const { override, adjustWorkbox } = require("customize-cra");
-module.exports = override(
-  // Override the GenerateSW configs.
-  adjustWorkbox((wb) => {
-    console.log(wb);
-    return Object.assign(wb, {
-      exclude: [...(wb.exclude || []), ...[/\.scss$/]],
-      globDirectory: "build",
-      globPatterns: [
-        ...(wb.globPatterns || []),
-        "/styles/**/*.css",
-        "styles/**/*.css",
-        "**/*.css",
-        "/**/*.css",
-      ],
-    });
-  }),
-);
+module.exports = function override(config, env) {
+  config.plugins = config.plugins.map((plugin) => {
+    if (plugin.constructor.name === "GenerateSW") {
+      return () => {};
+    } else {
+      return plugin;
+    }
+  });
+  return config;
+};
---
+++ src/sw.js
@@ -0,0 +1,42 @@
+if ("function" === typeof importScripts) {
+  importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
+  );
+  /* global workbox */
+  if (workbox) {
+    console.log("Workbox is loaded");
+
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+    /* custom cache rules*/
+    const handler = workbox.precaching.createHandlerBoundToURL("/index.html");
+    const navigationRoute = new workbox.routing.NavigationRoute(handler, {
+      denylist: [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp("^/_"),
+        // Exclude any URLs whose last part seems to be a file extension
+        // as they're likely a resource and not a SPA route.
+        // URLs containing a "?" character won't be blacklisted as they're likely
+        // a route with query params (e.g. auth callbacks).
+        new RegExp("/[^/?]+\\.[^/]+$"),
+      ],
+    });
+    workbox.routing.registerRoute(navigationRoute);
+
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      new workbox.strategies.CacheFirst({
+        cacheName: "images",
+        plugins: [
+          new workbox.expiration.ExpirationPlugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      }),
+    );
+  } else {
+    console.log("Workbox could not be loaded. No Offline support");
+  }
+}
