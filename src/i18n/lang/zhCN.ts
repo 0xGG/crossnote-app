cf212c85b32a6b675d655055a81d31d6cd4b541f
Yiyi Wang <shd101wyy@gmail.com>
2020-05-01 20:42:30 +0800

    fix: Fixed service worker skip waiting update

--- src/i18n/lang/zhCN.ts
+++ src/i18n/lang/zhCN.ts
@@ -220,7 +220,7 @@ export const zhCN = {
 
     // Notifications
     "notifications/refresh": "刷新来查看新的消息",
-    "notifications/new-version-available": "发现了新的应用版本可用更新！",
+    "notifications/new-version-available": "发现了新版本的交叉笔记！",
 
     // Settings
     "settings/about-this-project": "关于这个项目",
